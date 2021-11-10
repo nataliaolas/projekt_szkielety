0a2e88d07ca759c14dd69f32238fd6bf3db18895
Michal Jura <example@ail.com>
2021-11-10 12:44:26 +0100

    added admin and urls with basic view

--- planner/wedding/admin.py
+++ planner/wedding/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import AdditionalServices, Budget, Callendar, CeremonyPlace, Documents, Guest, Invitations, Music, Photographer, Timetable, WeddingHall
+
 # Register your models here.
+admin.site.register(Music)
+admin.site.register(Invitations)
+admin.site.register(Photographer)
+admin.site.register(CeremonyPlace)
+admin.site.register(AdditionalServices)
+admin.site.register(WeddingHall)
+admin.site.register(Guest)
+admin.site.register(Timetable)
+admin.site.register(Documents)
+admin.site.register(Budget)
+admin.site.register(Callendar)
