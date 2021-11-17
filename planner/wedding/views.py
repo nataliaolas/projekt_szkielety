c82665485a3769fb8f1362c1a38175447f8dba05
Natalia <nataliaolas7@gmail.com>
2021-11-17 15:27:35 +0100

    basic template added

--- planner/wedding/views.py
+++ planner/wedding/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
-def index(request):
-    template = loader.get_template('wedding/index.html')
+
+def main_cards(request):
+    template = loader.get_template('wedding/main_page.html')
     context = {'elo':'elo'}
     return HttpResponse(template.render(context, request))
+
+
+    
+
