e8c809eaa8f5bce96ef593c8b4271043705ac519
Michał Jura <71665711+MichalJura@users.noreply.github.com>
2021-11-23 20:31:13 +0100

    fixed

--- projekt_szkielety/planner/planner/urls.py
+++ projekt_szkielety/planner/planner/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('wedding', include('wedding.urls')),
+    path('', include('wedding.urls')),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
